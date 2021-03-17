8f303e3c99afb9fe23b6c93b717d555807369c0e
Stephen Meier <info@stephenmeier.net>
2021-03-17 19:56:05 -0400

    Update nb_default_param.py

--- asreviewcontrib/models/nb_default_param.py
+++ asreviewcontrib/models/nb_default_param.py
@@ -14,3 +14,22 @@ class NaiveBayesDefaultParamsModel(BaseTrainClassifier):
 
         super(NaiveBayesDefaultParamsModel, self).__init__()
         self._model = MultinomialNB()
+
+from asreview.models.feature_extraction.base import BaseFeatureExtraction
+
+class NaiveBayesFeatureExtration(BaseFeatureExtraction):
+    name = "nb_feat"
+
+    def __init__(self):
+
+        super(NaiveBayesFeatureExtration, self).__init__()
+
+    @abstractmethod
+    def transform(self, texts):
+        return texts
+    
+    def full_hyper_space(self):
+        from hyperopt import hp
+        hyper_choices = {}
+        hyper_space = {}
+        return hyper_space, hyper_choices
